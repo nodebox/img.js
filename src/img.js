015e23d8b65db7d5bfd67f37223f31fb29e94cdf
stebanos <stefan.gabriels@gmail.com>
2014-10-22 17:00:26 +0200

    Filter options are optional.

--- src/img.js
+++ src/img.js
@@ -344,18 +344,19 @@ Layer = function (data, type) {
 Layer.prototype.clone = function () {
     function cloneFilter(filter) {
         var key, value;
-        var f = {
-            name: filter.name,
-            options: {}
-        };
-        var optionsKeys = Object.keys(filter.options);
-        for (var i = 0; i < optionsKeys.length; i += 1) {
-            key = optionsKeys[i];
-            value = filter.options[key];
-            if (Array.isArray(value)) {
-                f.options[key] = value.slice(0);
-            } else {
-                f.options[key] = value;
+        var f = {};
+        f.name = filter.name;
+        if (filter.options !== undefined) {
+            f.options = {};
+            var optionsKeys = Object.keys(filter.options);
+            for (var i = 0; i < optionsKeys.length; i += 1) {
+                key = optionsKeys[i];
+                value = filter.options[key];
+                if (Array.isArray(value)) {
+                    f.options[key] = value.slice(0);
+                } else {
+                    f.options[key] = value;
+                }
             }
         }
         return f;
